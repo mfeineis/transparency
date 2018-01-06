b79dcc9a0ea3f5354ad30dff8d27c177d3a8187a
Martin Feineis <martinfeineis@gmx.de>
2018-01-06 23:49:42 +0100

    Rudimentary blocking of requests and storage handling, this may actually work out

--- shell/background.js
+++ shell/background.js
@@ -1,13 +1,19 @@
-/* global browser */
 /* global chrome */
 (function () {
+    chrome.tabs.query({'active': true}, function(tabs) {
+        //chrome.tabs.update(tabs[0].id, {url: newUrl});
+        console.log("chrome.tabs.query({active:true})", tabs, this);
+    });
+
+    // Doesn't work in Chrome??
+    //console.log("chrome.runtime.getBrowserInfo", chrome.runtime.getBrowserInfo());
 
     const views = chrome.extension.getViews();
 
     let incidentCount = 0;
 
     function logURL(requestDetails) {
-        console.log("Loading: " + requestDetails.url, views);
+        console.log("Loading: " + requestDetails.url, requestDetails, views);
 
         incidentCount += 1;
 
@@ -21,11 +27,40 @@
 
         const port = chrome.runtime.connect();
         port.postMessage({ type: "LOADING", payload: requestDetails });
+
+        return {
+            cancel: true, // Can only be set when not providing anything else in Chrome
+            //redirectUrl: requestDetails.url,
+            //redirectUrl: "https://38.media.tumblr.com/tumblr_ldbj01lZiP1qe0eclo1_500.gif",
+            //requestHeaders: requestDetails.requestHeaders,
+        };
     }
 
     chrome.webRequest.onBeforeRequest.addListener(
         logURL,
-        {urls: ["<all_urls>"]}
+        {urls: ["<all_urls>"], types: ["image"] },
+        ["blocking", "requestBody"], //, "requestHeaders"]
     );
+
+    /*
+      Log the storage area that changed,
+      then for each item changed,
+      log its old value and its new value.
+    */
+    function logStorageChange(changes, area) {
+        console.log("Change in storage area: " + area, changes);
+
+        const changedItems = Object.keys(changes);
+
+        for (const item of changedItems) {
+            console.log(item + " has changed:");
+            console.log("Old value: ");
+            console.log(changes[item].oldValue);
+            console.log("New value: ");
+            console.log(changes[item].newValue);
+        }
+    }
+
+    chrome.storage.onChanged.addListener(logStorageChange);
 
 }());
--- shell/popup.js
+++ shell/popup.js
@@ -2,12 +2,19 @@
 /* global Elm */
 (function () {
 
-    const bg = chrome.extension.getBackgroundPage();
+    //const bg = chrome.extension.getBackgroundPage();
 
     const app = Elm.Main.fullscreen();
 
+    const someValue = chrome.storage.local.get("some-key");
+    //storage.local.clear
+    //storage.local.get
+    //storage.local.remove
+    //storage.local.set
+    //storage.sync
+
     chrome.runtime.onConnect.addListener(function (port) {
-        console.log('[POPUP] chrome.runtime.onConnect', port.name, port, this);
+        console.log('[POPUP] (someValue ' + someValue + ') chrome.runtime.onConnect', port.name, port, this);
 
         port.onMessage.addListener(function (msg) {
             console.log("[POPUP] .. port.onMessage", msg);
